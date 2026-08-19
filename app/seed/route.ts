7fee776793f5b83303a466cb92fc4b865e333124
Juan Gabriel Valeriano <gabit690@gmail.com>
2026-08-18 22:38:27 -0300

    feat: apply streaming to Dashboard

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,10 +1,11 @@
-import bcrypt from 'bcrypt';
-import postgres from 'postgres';
-import { invoices, customers, revenue, users } from '../lib/placeholder-data';
+import bcrypt from "bcrypt";
+import postgres from "postgres";
+import { invoices, customers, revenue, users } from "../lib/placeholder-data";
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
-
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: false });
+debugger;
 async function seedUsers() {
+  console.log("seedUsers");
   await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
   await sql`
     CREATE TABLE IF NOT EXISTS users (
@@ -102,6 +103,7 @@ async function seedRevenue() {
 }
 
 export async function GET() {
+  debugger;
   try {
     const result = await sql.begin((sql) => [
       seedUsers(),
@@ -110,8 +112,9 @@ export async function GET() {
       seedRevenue(),
     ]);
 
-    return Response.json({ message: 'Database seeded successfully' });
+    return Response.json({ message: "Database seeded successfully" });
   } catch (error) {
+    console.log(error);
     return Response.json({ error }, { status: 500 });
   }
 }
